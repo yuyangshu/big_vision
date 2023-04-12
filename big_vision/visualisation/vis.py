a707deab66c2931a62359a0b7b833e759b70d15e
Yuyang Shu <yuyang.shu@gmail.com>
2023-04-12 20:41:47 +1000

    visualisation

---
+++ big_vision/visualisation/vis.py
@@ -0,0 +1,30 @@
+import json
+import matplotlib.pyplot as plt
+import sys
+
+
+def parse(filename):
+    steps, accuracy = [], []
+
+    with open (filename, 'r') as f:
+        for line in f.readlines():
+            entry = json.loads(line)
+            if (r"val/prec@1") in entry:
+                steps.append(entry["step"])
+                accuracy.append(entry[r"val/prec@1"])
+
+    return steps, accuracy
+
+
+if __name__ == "__main__":
+    filename = sys.argv[1] if len(sys.argv) > 1 else "big_vision_metrics.txt"
+
+    x, y = parse(filename)
+    x = [x * 0.8 for x in x]
+    x_b, y_b = parse("baseline.txt")
+
+    fig, ax = plt.subplots()
+    ax.plot(x, y)
+    ax.plot(x_b, y_b)
+
+    plt.savefig(f"{filename.split('.')[0]}-compare.png")
